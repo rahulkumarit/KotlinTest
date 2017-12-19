2ecc63fbf0692c6f938784062053bc2078db8bd9
Rahul Kumar <rahulkumar@devrepublic.nl>
2017-12-19 13:04:19 +0530

    This is first commit from android.

--- app/src/main/java/com/rnd/kotlintest/activities/SqliteActivity.kt
+++ app/src/main/java/com/rnd/kotlintest/activities/SqliteActivity.kt
@@ -1,6 +1,9 @@
 package com.rnd.kotlintest.activities
 
 import android.os.Bundle
+import android.widget.Toast
+import com.rnd.kotlintest.R
+import kotlinx.android.synthetic.main.activity_sqlite.*
 
 /**
  * Created by Devrepublic-14 on 12/19/2017.
@@ -9,6 +12,7 @@ class SqliteActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sqlite)
         try {
             initCoponets()
         } catch (e: Exception) {
@@ -18,7 +22,20 @@ class SqliteActivity : BaseActivity() {
 
     override fun initCoponets() {
         super.initCoponets()
+        btnSave.setOnClickListener {
+            saveDataDB()
+        }
     }
 
+    private fun saveDataDB() {
+        if (isValidData()) {
+            Toast.makeText(this, "Valid Data now save", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isValidData(): Boolean {
+        Toast.makeText(this, "Valid Data not now", Toast.LENGTH_SHORT).show()
+        return false
+    }
 
 }
